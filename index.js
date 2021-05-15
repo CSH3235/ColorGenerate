1e11708d58028ad8b76a4e2d92e6f466614a0f31
CSH <64762380+CSH3235@users.noreply.github.com>
2021-05-15 09:28:49 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 window.onload = function() {
 
-    var button = document.querySelector(".Btn")
+    var button = document.querySelector(".Btn");
+    var copyBtn = document.getElementById("copyBtn");
+
 
     function generateColor() {
             var randomColor = (Math.floor(Math.random()*16777216)).toString(16);
             console.log(randomColor)
             document.body.style.backgroundColor = "#" + randomColor;
-            var text = document.getElementById("text");
+            var text = document.getElementById("textDiv");
             text.innerText = "#" + randomColor;
     }
 
     button.addEventListener("click", function(){
-        generateColor()
+        generateColor();
+        copy();
     })
+
+    copyBtn.addEventListener("click", function() {
+        copy_to_clipboard();
+        copied();
+    })
+
+    
+    function copy_to_clipboard() {    
+        var text = document.getElementById("textDiv").innerText;
+        var createInput = document.createElement("input");
+        createInput.setAttribute("type", "text");
+        document.getElementById("textDiv").appendChild(createInput);
+        createInput.value = text;
+        createInput.select();
+        document.execCommand('copy');
+        document.getElementById("textDiv").removeChild(createInput);
+    }
+
+    function copied() {
+        var copied = document.getElementById("copyBtn");
+        copied.innerText="Copied!";
+    }
+    
+    function copy() {
+        var copy = document.getElementById("copyBtn");
+        copy.innerText="Copy";
+    }
 
 }
